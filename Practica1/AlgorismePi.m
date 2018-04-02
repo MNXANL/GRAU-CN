6b11ea52643ad82fa2617c3cb8bd533f4fdea4db
MNXANL <miguelmoregome@gmail.com>
2018-04-02 23:37:21 +0200

    as

---
+++ Practica1/AlgorismePi.m
@@ -0,0 +1,5 @@
+%% GRAU-CN | Pràctica A
+% Algorismes -> Exercici 1 (front-end)
+
+n = 5000;
+res = PiFunction(n)
---
+++ Practica1/TruncError2.m
@@ -0,0 +1,4 @@
+%% GRAU-CN | Pràctica A
+% Errors de truncament -> Exercici 2 (jocs de prova)
+
+X = InfinitePartialSums(16)
